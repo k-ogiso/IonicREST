b833bf8dc96deb98300cbe7a3852d586ad54faff
a-tachihara <a-tachihara@nri.co.jp>
2017-08-24 08:16:57 +0900

    bugfix

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,4 +1,4 @@
 export namespace Const {
-    export const API_BASE_URL = 'http://localhost:8100/todo/api/';
+    export const API_BASE_URL = '/todo/api/';
     export const API_VERSION = 'v1.0';
 }
