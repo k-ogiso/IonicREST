faf96d4199fe981336810da53659e601703d5658
a-hayashi@nebula.nri.co.jp <a-hayashi@nebula.nri.co.jp>
2017-08-24 15:17:41 -0700

    update home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { TaskServiceProvider } from '../../providers/task-service';
 import { Task } from '../../model/task';
+import { AddPage } from '../add/add';
 
 @Component({
   selector: 'page-home',
@@ -10,11 +11,13 @@ import { Task } from '../../model/task';
 export class HomePage {
 
   tasks: Task[];
+  
 
   constructor(
     public navCtrl: NavController,
     private taskService: TaskServiceProvider
   ) {
+    this.tasks=[];
     taskService.getTasks().subscribe(tasks => { this.tasks = tasks; });
   }
   addTask(task: Task): void {
@@ -22,5 +25,17 @@ export class HomePage {
   }
   delTask(task_id: number): void {
     this.taskService.delTask(task_id).subscribe();;
+  }
+
+  filt(rec:Task):boolean{
+    if (rec.status === "true"){
+      return false;
+    }else{
+      return true;
+    }
+    }
+   
+  goToAddPage() {
+  this.navCtrl.push(AddPage);
   }
 }
