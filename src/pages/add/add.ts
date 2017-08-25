b0a2309027487f9a64158f1200c44b339f4facae
nemoto0525 <nmt.yoshio@gmail.com>
2017-08-26 02:45:05 +0900

    レコメンドリスト

--- src/pages/add/add.ts
+++ src/pages/add/add.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { TaskServiceProvider } from '../../providers/task-service';
 import { Task } from '../../model/task';
 import { HomePage } from '../home/home';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 /**
  * Generated class for the AddPage page.
@@ -21,20 +22,24 @@ export class AddPage {
   item: string;
   endDate;
   errmsg: string;
+  closeResult: string;
+  fixedTaskList: string[] = ["pay", "call", "send", "check", "get", "goto"];
+  recomendTasks: string[] = [];
 
   constructor(
     public navCtrl: NavController,
-    private taskService: TaskServiceProvider
+    private taskService: TaskServiceProvider,
+    private modalService: NgbModal
   ) {
-
- }
+    // this.item.setFocus();
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddPage');
   }
 
   goToHomePage() {
-    this.navCtrl.push(HomePage);
+    this.navCtrl.pop(HomePage);
   }
 
   addTask(): void {
@@ -55,6 +60,29 @@ export class AddPage {
     this.taskService.addTask(task).subscribe();
 
     this.goToHomePage();
+  }
+
+  showRecomendItem(e) {
+
+    this.recomendTasks = [];
+
+    if (this.item.length != 0){
+      for (var i = 0; i < this.fixedTaskList.length; i++){
+        if (this.item.substr(0, this.item.length) == this.fixedTaskList[i].substr(0, this.item.length)) {
+          console.log(this.fixedTaskList[i]);
+          this.recomendTasks.push(this.fixedTaskList[i]);
+        }
+      }
+    }
+      console.log(e);
+
+    console.log(this.recomendTasks);
+
+  }
+
+  selectTask(task) {
+    console.log(task);
+    this.item = task;
   }
 
 }
